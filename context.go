af7fab58079a2c1909e5ea8937eb1932469ad95f
Bojan Delic <bojan@delic.in.rs>
2016-11-26 22:22:50 +0100

    SetClient private and rename GetClient to ClientFromContext.

--- context.go
+++ context.go
@@ -11,14 +11,14 @@ var clientKey clientKeyType = "client"
 
 // SetClient adds client instance to provided context and returns new
 // context with client value.
-func SetClient(ctx context.Context, client *http.Client) context.Context {
+func clientToContext(ctx context.Context, client *http.Client) context.Context {
 	c := context.WithValue(ctx, clientKey, client)
 	return c
 }
 
 // GetClient returns client instance from context. If client is not set, nil
 // will be returned.
-func GetClient(ctx context.Context) *http.Client {
+func ClientFromContext(ctx context.Context) *http.Client {
 	cl := ctx.Value(clientKey)
 	if cl == nil {
 		return nil
