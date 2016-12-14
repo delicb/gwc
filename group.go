6c82e35538d52f7a66c55df559ea14bceb6f744b
Bojan Delic <bojan@delic.in.rs>
2016-12-14 01:42:15 +0100

    Rename Layer to Group.

--- group.go
+++ group.go
@@ -6,41 +6,41 @@ import (
 	"go.delic.rs/cliware"
 )
 
-// Layer is container of any additional middlewares that group of endpoints use.
-type Layer struct {
+// Group is container of any additional middlewares that group of endpoints use.
+type Group struct {
 	Next  Doer
 	Chain *cliware.Chain
 }
 
-// NewLayer creates and returns new instance of Layer that will apply all provided
+// NewGroup creates and returns new instance of Group that will apply all provided
 // middlewares and use next Doer to do actual work.
-func NewLayer(next Doer, middlewares ...cliware.Middleware) *Layer {
-	return &Layer{
+func NewGroup(next Doer, middlewares ...cliware.Middleware) *Group {
+	return &Group{
 		Next:  next,
 		Chain: cliware.NewChain(middlewares...),
 	}
 }
 
 // Exec is implementation of cliware.Middleware interface.
-func (s *Layer) Exec(handler cliware.Handler) cliware.Handler {
+func (s *Group) Exec(handler cliware.Handler) cliware.Handler {
 	return s.Chain.Exec(handler)
 }
 
-// Use adds provided middlewares to this layers chain.
-func (s *Layer) Use(middleware ...cliware.Middleware) *Layer {
+// Use adds provided middlewares to this group's chain.
+func (s *Group) Use(middleware ...cliware.Middleware) *Group {
 	s.Chain.Use(middleware...)
 	return s
 }
 
 // Do applies all middlewares from this layer and provided middlewares and
 // calls next Doer to do actual work.
-func (s *Layer) Do(middlewares ...cliware.Middleware) (*Response, error) {
+func (s *Group) Do(middlewares ...cliware.Middleware) (*Response, error) {
 	return s.DoCtx(context.Background(), middlewares...)
 }
 
 // DoCtx applies all middlewares from this layer and provided middlewares and
 // calls next Doer to do actual work.
-func (s *Layer) DoCtx(ctx context.Context, middlewares ...cliware.Middleware) (*Response, error) {
+func (s *Group) DoCtx(ctx context.Context, middlewares ...cliware.Middleware) (*Response, error) {
 	// insert service itself to first place in middleware chain
 	middlewares = append(middlewares, nil)
 	copy(middlewares[1:], middlewares[0:])
